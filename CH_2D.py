0482ab918ef9e3acb87b4b68f21dbd1e835002ed
Enno <enno.i@web.de>
2025-05-10 23:57:01 +0200

    changed data loading and saving

--- CH_2D.py
+++ CH_2D.py
@@ -17,17 +17,22 @@ def inital_amb_seperated(X, Y):
 def initial_c_0_2D(X, Y, c_0=0.5):
     return (2.*c_0 - 1) + 0.001*np.random.standard_normal(size=(int(np.sqrt(X.size)), int(np.sqrt(Y.size))))
 
-def solve_ambplus_2D(phi_0=None, t_state = 0.0, t_end = 100.0, tau = 0.01, eps_val=1., a=-0.25, b=0.25, lam_val=1., zeta=4.0, D=0.2, M=1., s_start = -32.*np.pi, s_end = 32.*np.pi, s_N = 200):
+def solve_ambplus_2D(phi_0=None, t_state = 0.0, t_end = 100.0, tau = 0.01, eps_val=1., a=-0.25, b=0.25, lam_val=1.75, zeta=2.0, D=0.05, M=1., s_start = -32.*np.pi, s_end = 32.*np.pi, s_N = 200):
 
+    # log the parameters used:
+    if not os.path.exists("parameters.csv"):
+        with open("parameters.csv", 'w') as f:
+            f.write(f"t_state={t_state}, \nt_end={t_end}, \ntau={tau}, \neps_val={eps_val}, \na={a}, \nb={b}, \nlam_val={lam_val}, \nzeta={zeta}, \nD={D}, \nM={M}, \ns_start={s_start}, \ns_end={s_end}, \ns_N={s_N}")  
+    
     log_file = "log.csv"
 
     # Setup space discretization:
-    x = np.linspace(s_start, s_end, s_N, endpoint=False)         # real space
+    x = np.linspace(s_start, s_end, s_N, endpoint=False)                # real space
     y = np.linspace(s_start, s_end, s_N, endpoint=False)
-    X, Y = np.meshgrid(x, y)
-    kx = np.fft.fftfreq(s_N, d=(s_end - s_start)/s_N)*2.*np.pi    # fourier space
-    ky = np.fft.fftfreq(s_N, d=(s_end - s_start)/s_N)*2.*np.pi
-    KX, KY = np.meshgrid(kx, ky)
+    X, Y = np.meshgrid(x, y, indexing="ij")
+    kx = np.fft.fftfreq(s_N, d=(s_end - s_start)/s_N) *2*np.pi  	    # fourier space
+    ky = np.fft.fftfreq(s_N, d=(s_end - s_start)/s_N) *2*np.pi
+    KX, KY = np.meshgrid(kx, ky, indexing="ij")
     K_2 = KX**2 + KY**2
     K_4 = K_2**2
     #KXKY = KX * KY
@@ -35,13 +40,14 @@ def solve_ambplus_2D(phi_0=None, t_state = 0.0, t_end = 100.0, tau = 0.01, eps_v
     dy = Y[1][0] - Y[0][0]
     
     # Setup the phis for our time step with initial condition
-    try:
-        b = (phi_0 == None)
-        if b:
-            phi = initial_c_0_2D(X, Y, 0.4)
-    except:
+    if phi_0 is None:
+        phi = initial_c_0_2D(X, Y, 0.4)
+        prev_iter = 0
+    else:
         phi = phi_0
-    
+        data = np.loadtxt(log_file, delimiter=",", skiprows=1)
+        prev_iter = data[-1, 0]
+
     f_phi = np.fft.fft2(phi)
 
     # Setup time discretization
@@ -50,18 +56,18 @@ def solve_ambplus_2D(phi_0=None, t_state = 0.0, t_end = 100.0, tau = 0.01, eps_v
     # check every 10 steps
     check = int(t_N/10)   
 
+    # Setup the logging
     if not os.path.exists(log_file):
         with open(log_file, 'w') as f:
-            f.write("iteration,time,total_mass,phi_min,phi_max\n")   
+            f.write("iteration,time,total_mass,phi_min,phi_max\n")
 
     # Time stepping loop
-    for ii in range(t_N):
+    for ii in range(prev_iter, prev_iter+t_N):
 
         if ii % check == 0:
             # Save the result
-            out_file = f"phi_{ii}.csv"
-            np.savetxt(out_file, phi.real, delimiter=",")
-
+            out_file = f"phi_{ii}.npy"
+            np.save(out_file, phi.real)
             print(f"phi saved to {out_file}")
 
             with open(log_file, 'a') as f:
@@ -96,34 +102,35 @@ def solve_ambplus_2D(phi_0=None, t_state = 0.0, t_end = 100.0, tau = 0.01, eps_v
         f_phi = f_phi_new
 
     # print one last time
-    out_file = f"phi_{ii+1}.csv"
-    np.savetxt(out_file, phi.real, delimiter=",")
+    out_file = f"phi_{ii+1}.npy"
+    np.save(out_file, phi.real)
     print(f"phi saved to {out_file}")
+    with open(log_file, 'a') as f:
+        f.write(f"{ii+1},{t_state},{np.sum(phi.real)*dx*dy},{np.min(phi.real)},{np.max(phi.real)}\n")
 
 def solve_CH_2D(phi_0=None, t_state = 0.0, t_end = 1000.0, tau = 0.1, eps_val=0.01, lam_val=1., s_start = -32.*np.pi, s_end = 32.*np.pi, s_N = 200):
+    
     log_file = "log.csv"
 
     # Setup space discretization:
-    x = np.linspace(s_start, s_end, s_N, endpoint=False)         # real space
+    x = np.linspace(s_start, s_end, s_N, endpoint=False)                # real space
     y = np.linspace(s_start, s_end, s_N, endpoint=False)
-    X, Y = np.meshgrid(x, y)
-    kx = np.fft.fftfreq(s_N, d=(s_end - s_start)/s_N)*2.*np.pi    # fourier space
-    ky = np.fft.fftfreq(s_N, d=(s_end - s_start)/s_N)*2.*np.pi
-    KX, KY = np.meshgrid(kx, ky)
+    X, Y = np.meshgrid(x, y, indexing="ij")
+    kx = np.fft.fftfreq(s_N, d=(s_end - s_start)/s_N)*2*np.pi           # fourier space
+    ky = np.fft.fftfreq(s_N, d=(s_end - s_start)/s_N)*2*np.pi
+    KX, KY = np.meshgrid(kx, ky, indexing="ij")
     K_2 = KX**2 + KY**2
     K_4 = K_2**2
     dx = X[0][1] - X[0][0]
     dy = Y[1][0] - Y[0][0]
 
     # Setup the phis for our time step with initial condition
-    try:
-        b = (phi_0 == None)
-        print(b)
-        if b:
-            phi = initial_c_0_2D(X, Y, 0.5)
-
-    except:
+    if phi_0 is None:
+        phi = initial_c_0_2D(X, Y, 0.4)
+    else:
         phi = phi_0
+        data = np.loadtxt(log_file, delimiter=",", skiprows=1)
+        prev_iter = data[-1, 0]
 
     f_phi = np.fft.fft2(phi)
 
@@ -138,12 +145,12 @@ def solve_CH_2D(phi_0=None, t_state = 0.0, t_end = 1000.0, tau = 0.1, eps_val=0.
             f.write("iteration,time,total_energy,total_mass,phi_min,phi_max\n")    
 
     # Time stepping loop
-    for ii in range(t_N):
+    for ii in range(prev_iter, prev_iter+t_N):
 
         if ii % check == 0:
             # Save the result
-            out_file = f"phi_{ii}.csv"
-            np.savetxt(out_file, phi.real, delimiter=",")
+            out_file = f"phi_{ii}.npy"
+            np.save(out_file, phi.real)
             print(f"phi saved to {out_file}")
 
             dphi_dx = np.fft.ifft2(1j * KX * f_phi).real
@@ -169,9 +176,17 @@ def solve_CH_2D(phi_0=None, t_state = 0.0, t_end = 1000.0, tau = 0.1, eps_val=0.
         f_phi = f_phi_new
 
     # print one last time
-    out_file = f"phi_{ii+1}.csv"
-    np.savetxt(out_file, phi.real, delimiter=",")
+    out_file = f"phi_{ii+1}.npy"
+    np.save(out_file, phi.real)
     print(f"phi saved to {out_file}")
+
+    dphi_dx = np.fft.ifft2(1j * KX * f_phi).real
+    dphi_dy = np.fft.ifft2(1j * KY * f_phi).real
+    grad_phi_2 = dphi_dx**2 + dphi_dy**2
+    E = np.sum(eps_val/2. * grad_phi_2 + lam_val/4. * (phi.real**2 - 1.)**2) * dx*dy
+
+    with open(log_file, 'a') as f:
+            f.write(f"{ii+1},{t_state},{E},{np.sum(phi.real)*dx*dy},{np.min(phi.real)},{np.max(phi.real)}\n")
     
 
 def main():
@@ -181,19 +196,21 @@ def main():
         filename = sys.argv[1]
 
         try:
-            phi_0 = np.loadtxt(filename, delimiter=",")
+            phi_0 = np.load(filename)
         except Exception as e:
             print(f"Error reading file: {e}")
             sys.exit(1)
 
         print(f"Loaded {filename} with shape {phi_0.shape}")
+        N = phi_0.shape[0]
     else:
         phi_0 = None
+        N = 200
 
     np.random.seed(0)
-
-    # Solve a equation
-    solve_ambplus_2D(phi_0, tau=0.001, t_end=100, s_N=200)
+    
+    # Solve an equation
+    solve_ambplus_2D(phi_0, s_N=N, tau=0.02, t_end=400)
 
 if __name__ == "__main__":
     main()
--- plot_phis.py
+++ plot_phis.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
 import matplotlib.animation as animation
 
 s_start = -32*np.pi
@@ -9,20 +8,21 @@ s_end = 32*np.pi
 fig, ax = plt.subplots()
 
 # Read the first frame to initialize the plot and colorbar
-phi0 = pd.read_csv("phi_0.csv", header=None).values
+phi0 = np.load("phi_0.npy")
 im = ax.imshow(phi0, extent=[s_start, s_end, s_start, s_end], cmap='inferno', vmin=-1, vmax=1)  # set fixed vmin/vmax if possible
 cbar = fig.colorbar(im, ax=ax)
 title = ax.set_title("Iteration = 0")
 
 # List of frame indices
-k_values = list(range(0, 1+100000, 10000))
+k_values = list(range(0, 1+20000, 2000))
 
 # Animation update function
 def update(frame):
     k = k_values[frame]
-    phi = pd.read_csv(f"phi_{k}.csv", header=None).values
+    phi = np.load(f"phi_{k}.npy")
     im.set_array(phi)
 
+    # for debugging purposes
     # vmin, vmax = phi.min(), phi.max()
     # im.set_clim(vmin, vmax)        # Update the image color scale
     # cbar.update_normal(im)         # Redraw colorbar to reflect new limits
